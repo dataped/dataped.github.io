6350c786082e4945ce28f3c75edf37d8766eb83a
Rolly Maulana Awangga <awangga@gmail.com>
2023-12-10 08:26:04 +0700

    oke

---
+++ index.js
@@ -0,0 +1,26 @@
+function uploadImage() {
+    const input = document.getElementById('imageInput');
+    const file = input.files[0];
+
+    if (!file) {
+        alert('Please select an image file');
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('image', file);
+
+    fetch('http://localhost:3000/upload', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        alert('Image uploaded successfully');
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Error uploading image');
+    });
+}
